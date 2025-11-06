744b90aebf4a327a6112eeb291065d05ea664383
Bondada-Vighnesh <bondada.vighnesh@capgemini.com>
2025-11-07 00:10:09 +0530

    Updated EmployeeService.java

--- src/main/java/com/cg/sprint/service/EmployeeService.java
+++ src/main/java/com/cg/sprint/service/EmployeeService.java
@@ -1,11 +1,13 @@
 package com.cg.sprint.service;
 
 import com.cg.sprint.dto.EmployeeDTO;
+import com.cg.sprint.dto.EmployeeSummaryDto;
 import java.util.List;
 
 public interface EmployeeService {
-    List<EmployeeDTO> getAllEmployees();
-    EmployeeDTO getEmployeeById(Long id);
-    List<EmployeeDTO> getEmployeesByLocation(Long locationId);
+  List<EmployeeDTO> getAllEmployees();
+  EmployeeDTO getEmployeeById(Long id);
 
+  // NEW
+  List<EmployeeSummaryDto> getEmployeeSummariesByCountry(String countryName);
 }
