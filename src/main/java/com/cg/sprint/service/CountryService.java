47fd9178e6083c6d8f9504612c8f4e632147a03b
Bondada-Vighnesh <bondada.vighnesh@capgemini.com>
2025-11-07 00:09:38 +0530

    Updated CountryService.java

--- src/main/java/com/cg/sprint/service/CountryService.java
+++ src/main/java/com/cg/sprint/service/CountryService.java
@@ -2,10 +2,14 @@ package com.cg.sprint.service;
 
 import com.cg.sprint.dto.CountryDTO;
 import com.cg.sprint.dto.LocationDTO;
+import com.cg.sprint.dto.RegionWithCountriesDto;
 import java.util.List;
 
 public interface CountryService {
-    List<CountryDTO> getAllCountries();
-    CountryDTO getCountryById(String id);
-    List<LocationDTO> getLocationsByCountry(String countryId);
+  List<CountryDTO> getAllCountries();
+  CountryDTO getCountryById(String id);
+  List<LocationDTO> getLocationsByCountry(String countryId);
+
+  // NEW
+  List<RegionWithCountriesDto> getRegionsWithCountries();
 }
